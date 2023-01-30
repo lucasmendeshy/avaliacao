1927e30bde5bd43a31cbb9707f441a715addf760
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-01-30 09:32:59 -0300

    shared

--- App.js
+++ App.js
@@ -1,43 +1,43 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Pressable, Alert } from "react-native";
-import * as StoreReview from "expo-store-review";
-import { Linking } from "react-native";
+import * as Sharing from "expo-sharing";
 
 export default function App() {
-  /* const [count, setCount] = useState(0);
-  const botao = () => setCount(prevCount => prevCount + 1); */
+  /*  if (!(await Sharing.isAvailableAsync())) {
+    alert(`Uh oh, sharing isn't available on your platform`);
+    return;
+  }
+  // console.log(getImageLocalAddress);
+  await Sharing.shareAsync(fileAddress, {
+    mimeType: "image/jpeg",
+    dialogTitle: "Share Your Result",
+  }); */
 
-  const itunesItemId = 982107779;
-  // Abra a App Store do iOS no navegador -> redireciona para a App Store no iOS
-  Linking.openURL(
-    `https://apps.apple.com/app/apple-store/id${itunesItemId}?action=write-review`
-  );
-  // Abra a App Store do iOS diretamente
-  Linking.openURL(
-    `itms-apps://itunes.apple.com/app/viewContentsUserReviews/id${itunesItemId}?action=write-review`
-  );
-  /* 
-  const review = async () => {
-    if (StoreReview.isAvailableAsync()) {
-      await StoreReview.requestReview()
-        .then(function (response) {
-          Alert.alert("responda-me", response);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+  /*   if (!Sharing.isAvailableAsync()) {
+    Alert.alert("Ops!", "compartilhamento não está disponível");
+  }
+  await Sharing.shareAsync(fileAddress, {
+    mimeType: "image/jpeg",
+    dialogTitle: "compartilhe seu resultado",
+  }); */
+
+  const compartilhe = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
     }
-  }; */
+    // console.log(getImageLocalAddress);
+    await Sharing.shareAsync(fileAddress, {
+      mimeType: "image/jpeg",
+      dialogTitle: "Share Your Result",
+    });
+  };
 
   return (
     <View style={estilos.container}>
       <StatusBar style="auto" />
-      <View style={estilos.countContainer}>
-        <Text>Count: </Text>
-      </View>
-
-      <Pressable style={estilos.botao} onPress={review}>
+      <Pressable style={estilos.botao} onPress={compartilhe}>
         <Text style={estilos.botaoTexto}>AVALIE</Text>
       </Pressable>
     </View>
@@ -62,9 +62,5 @@ const estilos = StyleSheet.create({
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
-  },
-  countContainer: {
-    alignItems: "center",
-    padding: 10,
   },
 });
