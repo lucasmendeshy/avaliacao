efef1e78e52ef9f63b5e17f4fef356e548a2b070
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-01-30 08:26:27 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,45 +1,35 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
-import * as StoreReview from 'expo-store-review';
-import { Linking } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Pressable, Alert } from "react-native";
+import * as StoreReview from "expo-store-review";
+import { Linking } from "react-native";
 
 export default function App() {
-   /* const [count, setCount] = useState(0);
+  /* const [count, setCount] = useState(0);
   const botao = () => setCount(prevCount => prevCount + 1); */
 
-
-  /* const itunesItemId = 982107779;
+  const itunesItemId = 982107779;
   // Abra a App Store do iOS no navegador -> redireciona para a App Store no iOS
-  Linking.openURL(`https://apps.apple.com/app/apple-store/id${itunesItemId}?action=write-review`);
+  Linking.openURL(
+    `https://apps.apple.com/app/apple-store/id${itunesItemId}?action=write-review`
+  );
   // Abra a App Store do iOS diretamente
   Linking.openURL(
     `itms-apps://itunes.apple.com/app/viewContentsUserReviews/id${itunesItemId}?action=write-review`
-  ); */
-
-/* 
-  handleReview = async () => {
-    if (StoreReview.isAvailableAsync()) {
-     await StoreReview.requestReview()
-     .then(function(response){
-       Alert.alert("responda-me", response)
-      })
-     .catch(e => { console.log(e) })
-    }
-  } */
-
+  );
+  /* 
   const review = async () => {
     if (StoreReview.isAvailableAsync()) {
       await StoreReview.requestReview()
-      .then(function(response){
-        Alert.alert("responda-me", response)
-       })
-      .catch(e => { console.log(e) })
-     }
-  }
+        .then(function (response) {
+          Alert.alert("responda-me", response);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  }; */
 
-    
-  
   return (
     <View style={estilos.container}>
       <StatusBar style="auto" />
@@ -47,11 +37,9 @@ export default function App() {
         <Text>Count: </Text>
       </View>
 
-        <Pressable style={estilos.botao} onPress={review}>
-              <Text style={estilos.botaoTexto}>
-                AVALIE
-              </Text>
-        </Pressable>
+      <Pressable style={estilos.botao} onPress={review}>
+        <Text style={estilos.botaoTexto}>AVALIE</Text>
+      </Pressable>
     </View>
   );
 }
@@ -59,16 +47,16 @@ export default function App() {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-    botao: {
+  botao: {
     padding: 14,
     backgroundColor: "purple",
     width: "90%",
     alignItems: "center",
-    borderRadius: 4
+    borderRadius: 4,
   },
   botaoTexto: {
     color: "white",
@@ -76,7 +64,7 @@ const estilos = StyleSheet.create({
     fontWeight: "bold",
   },
   countContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
   },
 });
