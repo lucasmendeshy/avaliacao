91fab582241ae3b1f5801f3746c00f8c788dc2c7
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-02-05 10:53:19 -0300

    instalação

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, Alert, ScrollView } from "react-native";
+import { StyleSheet, Text, View} from "react-native";
 
 export default function App() {
 
 
   return (
-  
     <View style={estilos.container}>
       <StatusBar style="auto" />
-      
-  
-<ScrollView>
-  <Text allowFontScaling={true} maxFontSizeMultiplier={1.5}  style={estilos.titulo}>LOREM IPSUM</Text>
-  <Text allowFontScaling={true} maxFontSizeMultiplier={1.2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+      <Text style={estilos.title}>Lorem Ipsum</Text>
 
-  <Text allowFontScaling={true} maxFontSizeMultiplier={1.2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
-
-      </ScrollView>
+     
       </View>
   );
 }
@@ -25,17 +18,11 @@ export default function App() {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    marginVertical: 40,
+    backgroundColor: "#000",
     padding: 8
   },
-  titulo: {
-    textAlign: "center",
-    fontSize: 24,
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    marginVertical: 8
+  title: {
+    fontSize: 12,
   }
+
 });
