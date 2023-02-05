a81eed1637170e91f0d41f89f474f4bccfc424b1
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-02-05 10:01:26 -0300

    Propriedades do componente Text

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, Alert } from "react-native";
+import { StyleSheet, Text, View, Pressable, Alert, ScrollView } from "react-native";
 
 export default function App() {
 
 
   return (
+  
     <View style={estilos.container}>
       <StatusBar style="auto" />
+      
   
+<ScrollView>
+  <Text allowFontScaling={true} maxFontSizeMultiplier={1.5}  style={estilos.titulo}>LOREM IPSUM</Text>
+  <Text allowFontScaling={true} maxFontSizeMultiplier={1.2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
 
-      <Pressable style={estilos.botao}>
-        <Text style={estilos.botaoTexto}>AVALIE</Text>
-      </Pressable>
-    </View>
+  <Text allowFontScaling={true} maxFontSizeMultiplier={1.2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+
+      </ScrollView>
+      </View>
   );
 }
 
@@ -23,21 +28,14 @@ const estilos = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    marginVertical: 40,
+    padding: 8
   },
-  botao: {
-    padding: 14,
-    backgroundColor: "purple",
-    width: "90%",
-    alignItems: "center",
-    borderRadius: 4,
-  },
-  botaoTexto: {
-    color: "white",
-    fontSize: 16,
+  titulo: {
+    textAlign: "center",
+    fontSize: 24,
     fontWeight: "bold",
-  },
-  countContainer: {
-    alignItems: "center",
-    padding: 10,
-  },
+    textTransform: "uppercase",
+    marginVertical: 8
+  }
 });
