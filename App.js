8c49d245e00cb7aee4511f2df3290b24877ce35d
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-02-06 09:57:46 -0300

    Contador de caracteres

--- App.js
+++ App.js
@@ -1,42 +1,39 @@
 import { useState, createRef, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Alert} from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert,
+  TextInput,
+} from "react-native";
 import Input from "./components/input";
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const emailInput = createRef();
-  const senhaInput = createRef();
+  const [text, setText] = useState("");
 
-  useEffect(()=>emailInput.current.resetError(),[email])
-  useEffect(()=>senhaInput.current.resetError(),[senha])
-
-  function logar () {
-    if (email == '') {
-      Alert.alert('E-mail inválido', 'Tente novamente!');
-       emailInput.current.focusOnError();
-       return; 
+  const handleTextChange = (inputText) => {
+    if (inputText.length <= 250) {
+      setText(inputText);
     }
-    if (senha == '') {
-      Alert.alert('Senha inválida', 'Tente novamente!');
-       senhaInput.current.focusOnError(); 
-       return;
-    }
-  }
-
+  };
 
   return (
     <SafeAreaView style={estilos.viewSafe}>
-      <StatusBar bartstyle="dark"/>
-      <Input ref={emailInput} iconName={'account-outline'} placeholder= "E-mail" autoCapitalize="none"  autoCorrect={false} keyboardType="email-address" value={email} onChangeText={setEmail} />
-
-
-
-     <Input ref={senhaInput} iconName={'lock-outline'} secureTextEntry placeholder= "Senha"  autoCapitalize="none" autoCorrect={false}  keyboardType="default" value={senha}  onChangeText={setSenha}/>
-     <TouchableOpacity onPress={logar}>
-     <Text>Logar</Text>
-      </TouchableOpacity> 
+      <StatusBar bartstyle="dark" />
+      <View>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          onChangeText={handleTextChange}
+          value={text}
+        />
+        {text.length > 250 && (
+          <Text style={{ color: "red" }}>Max 300 characters allowed</Text>
+        )}
+        <Text>{text.length} characters</Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -44,7 +41,6 @@ export default function App() {
 const estilos = StyleSheet.create({
   viewSafe: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
   },
-
 });
