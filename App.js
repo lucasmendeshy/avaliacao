b852f452669629c0c6a3f8443459830acafe1d5d
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-02-06 08:34:07 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,52 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, Alert, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Alert,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
-
-
   return (
-  
     <View style={estilos.container}>
       <StatusBar style="auto" />
-      
-  
-<ScrollView>
-  <Text allowFontScaling={true} maxFontSizeMultiplier={1.5}  style={estilos.titulo}>LOREM IPSUM</Text>
-  <Text allowFontScaling={true} maxFontSizeMultiplier={1.2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
 
-  <Text allowFontScaling={true} maxFontSizeMultiplier={1.2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+      <ScrollView>
+        <Text
+          allowFontScaling={true}
+          maxFontSizeMultiplier={0}
+          style={estilos.titulo}
+        >
+          LOREM IPSUM
+        </Text>
+        <Text allowFontScaling={true} maxFontSizeMultiplier={1.3}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </Text>
 
+        <Text allowFontScaling={true} maxFontSizeMultiplier={1.2}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </Text>
       </ScrollView>
-      </View>
+    </View>
   );
 }
 
@@ -29,13 +57,13 @@ const estilos = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginVertical: 40,
-    padding: 8
+    padding: 8,
   },
   titulo: {
     textAlign: "center",
     fontSize: 24,
     fontWeight: "bold",
     textTransform: "uppercase",
-    marginVertical: 8
-  }
+    marginVertical: 8,
+  },
 });
